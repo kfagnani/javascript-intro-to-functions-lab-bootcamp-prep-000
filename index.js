ee438b7ac0589799bc43b9f16d4ca00a04b895ef
Kirstyn Fagnani <kirstyn.fagnani@gmail.com>
2019-05-28 04:20:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,14 +12,15 @@ function logWhisper(string) {
   console.log("hello")
 }
 
-function sayHiToGrandma (string) {
-  if (string === "hello") 
+function sayHiToGrandma(string) {
+  if (string === "hello") {
     return "I can't hear you!"
   }
   else if (string === "HELLO") {
     return "YES INDEED!"
   }
-  else if (string === "I love you, Grandma."){
-return "I love you, too."
-    
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
   }
