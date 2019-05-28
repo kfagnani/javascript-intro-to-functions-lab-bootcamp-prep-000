2cc07f9db7c6256549bae918090aaaa404a44339
Kirstyn Fagnani <kirstyn.fagnani@gmail.com>
2019-05-28 04:08:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function sayHiToGrandma(string) {
 if (string.toLowerCase() === string)
 return "I can't hear you!"
 }
-  if (string.toUpperCase() === string)
-  {return "YES INDEED!"
-}
+  if (string.toUpperCase() === string) {
+   return "YES INDEED!"
+  }
