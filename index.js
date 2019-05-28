bbd281f3a8adfa536c57d447dbd8bc32cb9ec308
Kirstyn Fagnani <kirstyn.fagnani@gmail.com>
2019-05-28 04:10:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,15 @@ function logShout(string) {
 function logWhisper(string) {
   console.log("hello")
 }
-function sayHiToGrandma(string) {
-if (string.toLowerCase() === string)
-return "I can't hear you!"
-}
+ function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) 
+    return "I can't hear you!"
+  }
+
   if (string.toUpperCase() === string) {
-   return "YES INDEED!"
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
   }
